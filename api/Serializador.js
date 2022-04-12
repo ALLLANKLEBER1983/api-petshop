b4b7eb2d265cdb315a7afc5bdc70f382ff27a0a7
Allan Kleber <allankleber1983@gmail.com>
2022-04-11 22:41:01 -0300

    adição da chamado dos dados por xml

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,15 +1,31 @@
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const jsontoxml = require('jsontoxml')
 
 class Serializador {
     json(dados){
         return JSON.stringify(dados)
     }
 
+    xml(dados){
+        let tag = this.tagSingular
+        if(Array.isArray(dados)){
+            tag = this.tagPlural
+            dados = dados.map((item) =>{
+                return {
+                    [this.tagSingular]:item
+                }
+            })
+        }
+        return jsontoxml({[tag]: dados})
+    }
+
     serializar (dados){
+        dados = this.filtrar(dados)
         if (this.contentType === 'application/json'){
-        return this.json(
-            this.filtrar(dados)
-        )
+        return this.json(dados)
+        }
+        if(this.contentType === 'application/xml'){
+            return this.xml(dados)
         }
 
         throw new ValorNaoSuportado(this.contentType)
@@ -47,6 +63,8 @@ class SerializadorFornecedor extends Serializador {
             'empresa',
             'categoria'
         ].concat(camposExtras || [])
+        this.tagSingular = 'fornecedor'
+        this.tagPlural = 'fornecedores'
     }
 }
 
@@ -58,6 +76,8 @@ class SerializadorErro extends Serializador {
             'id',
             'mensagem'
         ].concat(camposExtras || [])
+        this.tagSingular = 'erro'
+        this.tagPlural = 'erros'
 
     }
     
@@ -67,5 +87,5 @@ module.exports = {
     Serializador:Serializador,
     SerializadorFornecedor:SerializadorFornecedor,
     SerializadorErro:SerializadorErro,
-    formatosAceitos: ['application/json']
+    formatosAceitos: ['application/json', 'application/xml']
 }
